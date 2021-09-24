99fedeb5fdc2c513e5251be0c95019388ceb12e2
y.pavlov <lenvonder12vorh@gmail.com>
2021-09-24 19:25:34 +0300

    Added polygon, @truffle/hdwallet-provider

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,39 @@
 const path = require('path')
+const fs = require('fs');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+let secrets = JSON.parse(fs.readFileSync('./secrets.json'));
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "vapp/src/contracts"),
+
+  networks: {
+
+    dev: { //poligon  local fork
+      host: "127.0.0.1",
+      port: 8555,
+      network_id: "137"
+    },
+
+    polygon: {
+      provider: function() {
+        return new HDWalletProvider(
+            secrets.polygon.pk,
+            secrets.polygon.infura
+        )
+      },
+      network_id: 137,
+      gasPrice: 15000000000,
+      networkCheckTimeout: 1000000000,
+    },
+
+
+  },
+
+  compilers: {
+    solc: {
+      version: "0.8.6"
+    }
+  },
 
 };
