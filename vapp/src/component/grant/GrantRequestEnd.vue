bf1827c4a2cc20302fa5d7d67056f348cc34701e
y.pavlov <lenvonder12vorh@gmail.com>
2021-09-25 22:43:10 +0300

    Add qr code to last step

--- vapp/src/component/grant/GrantRequestEnd.vue
+++ vapp/src/component/grant/GrantRequestEnd.vue
@@ -17,13 +17,20 @@
           <v-col class="form  fixed" lg="4">
             <v-card style="height: 100%">
               <v-card-title>
-                Заявка успешно создана!
+              </v-card-title>
+              <v-card-title primary-title class="justify-center">
+                <div>
+                  <h5 class="headline text--accent-2">Заявка успешно отправлена!</h5>
+                  <h3>Номер заявки № 16</h3>
+                </div>
               </v-card-title>
               <v-card-text>
-                  <span>Номер вашей заявки № 16</span>
-
+                <v-row justify="center" align="center">
+                  <div>
+                    <v-img class="text-center" width="250" height="250" :src="require('../../assets/qr-code.jpg')"/>
+                  </div>
+                </v-row>
                 <v-btn outlined class="btn-get mt-10" color="black" @click="goToNext">Открыть</v-btn>
-
               </v-card-text>
             </v-card>
 
@@ -47,7 +54,10 @@ export default {
   methods: {
 
     goToNext() {
-
+      this.$router.push({
+        name: 'Detail',
+        params: {id: '0x1bbe067bb0fc732188f5bc487a195a3b7d94e743fb0011834a73a38d662f581f'}
+      })
     },
   }
 }
