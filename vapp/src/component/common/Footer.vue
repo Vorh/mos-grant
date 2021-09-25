0ddfac6bb10c5dd73d19f29402f58170c4b7691a
alexander <a.sharuev@ifrbanking.ru>
2021-09-25 08:47:27 +0300

    Footer changed

--- vapp/src/component/common/Footer.vue
+++ vapp/src/component/common/Footer.vue
@@ -1,44 +1,53 @@
 <template>
-  <v-footer
-      padless
-  >
-    <v-card
-        class="flex"
-        flat
-        tile
-    >
-      <v-card-text>
-        <v-row justify="end">
-          <v-col lg="10"></v-col>
-          <v-col lg="2">
-            <v-btn icon  target="_blank">
-              <v-icon color="black" >mdi-github</v-icon>
-            </v-btn>
+    <v-footer dark padless>
+        <v-card
+                class="footer-row white--text text-center"
+                width="100%"
+                flat
+                tile
+        >
+            <v-card-text>
+                <v-btn icon target="_blank">
+                    <v-icon color="black">mdi-github</v-icon>
+                </v-btn>
 
-            <v-btn icon  target="_blank">
-              <v-icon color="black" >mdi-discord</v-icon>
-            </v-btn>
+                <v-btn icon target="_blank">
+                    <v-icon color="black">mdi-discord</v-icon>
+                </v-btn>
 
-            <v-btn icon  target="_blank">
-              <v-icon color="black" >mdi-twitter</v-icon>
-            </v-btn>
+                <v-btn icon target="_blank">
+                    <v-icon color="black">mdi-twitter</v-icon>
+                </v-btn>
 
-            <v-btn icon  target="_blank">
-              <v-icon color="black" >mdi-email</v-icon>
-            </v-btn>
-          </v-col>
-        </v-row>
-      </v-card-text>
-    </v-card>
-  </v-footer>
+                <v-btn icon target="_blank">
+                    <v-icon color="black">mdi-email</v-icon>
+                </v-btn>
+            </v-card-text>
+
+            <v-card-text class="white--text pt-0">
+                <!-- TODO: footer text -->
+                Footer text
+            </v-card-text>
+
+            <v-divider></v-divider>
+
+            <v-card-text class="white--text">
+                {{ new Date().getFullYear() }} — <strong>mos-grant</strong>
+            </v-card-text>
+        </v-card>
+    </v-footer>
 </template>
 
 <script>
 export default {
-  name: "Footer"
+    name: "Footer"
 }
 </script>
 
-<style scoped>
+<style>
+
+.footer-row {
+    background-color: #2C2C2C;
+}
 
 </style>
